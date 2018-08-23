ee133e0d4b2cd5741ffc357f3ef4ab7e92294e35
Jed Peek <jedpeek@yahoo.com>
2018-08-23 23:37:07 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -16,8 +16,8 @@ class Student < ActiveRecord::Base
 
   def self.search(search)
     if search
-      
-      find(:all, conditions: ['NAME like ?', "%#{search}%"])
+
+      find(:all, conditions: ['name LIKE ?', "%#{search}%"])
     else
       self.all
     end
